06fe884fc22f8ece64ebf835ca560641725c6cb5
tombrushwood <tom@hellogrowth.co.uk>
2018-06-27 00:51:28 +0100

    add support and interaction for main call to action section

--- static/js/custom.js
+++ static/js/custom.js
@@ -37,4 +37,57 @@ $(document).ready(function() {
     }
   });
 
+  // HANDLE CTA FUNCTIONS
+  var arrowWidth = 20;
+  $.fn.resizeselect = function(settings) {
+    return this.each(function() {
+      $(this).change(function(){
+        var $this = $(this);
+        // create test element
+        var text = $this.find("option:selected").text();
+        var $test = $("<span>").html(text).css({
+          "font-size": $this.css("font-size"), // ensures same size text
+          "visibility": "hidden" 							 // prevents FOUC
+        });
+        // add to body, get width, and get out
+        $test.appendTo($this.parent());
+        var width = $test.width();
+        $test.remove();
+        // set select width
+        $this.width(width + arrowWidth);
+        // run on start
+      }).change();
+    });
+  };
+  // run by default
+  $("select.resizeselect").resizeselect();
+  $("select.resizeselect").on('click', function(){
+    $(this).parent().addClass('chosen');
+  });
+
+  // HANDLE HOMEPAGE TOP ANIMATION
+  setTimeout(function(){
+    $('.laptop-illustration').addClass('active');
+    $('.laptop-illustration .img-to-float.delay-0').addClass('animate-float');
+    setTimeout(function(){
+      $('.laptop-illustration .img-to-float.delay-1').addClass('animate-float');
+    }, 750);
+  }, 300);
+
+
+
+  // HANDLE COMPANY HISTORY SELECT
+  $(function(){
+  	var inputs = $('.timeline-date');
+  	var paras = $('.timeline-info-container').find('p');
+  	$(inputs).click(function(){
+  		var t = $(this),
+  				ind = t.index(),
+  				matchedPara = $(paras).eq(ind);
+
+  		$(t).add(matchedPara).addClass('active');
+  		$(inputs).not(t).add($(paras).not(matchedPara)).removeClass('active');
+  	});
+  });
+
 });
